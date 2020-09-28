0325c8d9dd54b179babc93b870ddb9a5adfbec01
Zeebo0 <1363667093@qq.com>
2020-09-28 13:56:15 +0800

    correct AuthorizeController

--- src/main/java/com/zhangzebo/controller/AuthorizeController.java
+++ src/main/java/com/zhangzebo/controller/AuthorizeController.java
@@ -4,26 +4,42 @@ import com.zhangzebo.dto.AccessTokenDTO;
 import com.zhangzebo.dto.GitHubUser;
 import com.zhangzebo.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secrect}")
+    private String clientSecrect;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam("code") String code, @RequestParam("state") String state) {
+    public String callback(@RequestParam("code") String code, @RequestParam("state") String state, HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("cefddbb5756bb3e83b2c");
-        accessTokenDTO.setClient_secret("e2d8b5813cf12c3744511cc5dec148aac4f36cb9");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecrect);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            //  登录成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:index";
+        } else {
+            //  登录失败，重新登录
+            return "redirect:index";
+        }
     }
 }
